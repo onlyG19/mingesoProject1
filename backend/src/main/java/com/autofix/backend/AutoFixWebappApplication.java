549214795cd00a6c7710ac02a848b43f9ba021de
onlyG19 <byron.gracia@usach.cl>
2024-04-26 21:49:42 -0400

    first commit springboot

---
+++ backend/src/main/java/com/autofix/backend/AutoFixWebappApplication.java
@@ -0,0 +1,13 @@
+package com.autofix.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFixWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoFixWebappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/autofix/backend/AutoFixWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autofix.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoFixWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
